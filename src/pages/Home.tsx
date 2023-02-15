371b42bf96695715ef6434cc9f774b6fe0c37169
Vaibhav Matere <vpm00718897@gmail.com>
2023-02-15 18:36:09 +0530

    style : navbar and sidebar alignment

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 function Home() {
-     return (
-          <div className="text-3xl font-bold underline">Home</div>
-     )
+  return (
+    <>
+      <div className="max-h-screen overflow-hidden">
+        <div style={{ height: "7.5vh" }}>
+          <Navbar />
+        </div>
+      </div>
+      <div className="flex" style={{ height: "92.5vh" }}>
+        <Sidebar />
+      </div>
+    </>
+  );
 }
 export default Home;
